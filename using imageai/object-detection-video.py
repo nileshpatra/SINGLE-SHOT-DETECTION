2d101f765a2fb12b98abf4c52ba9a0847fb8a3de
NILESH <37436956+nileshpatra@users.noreply.github.com>
2019-03-19 23:23:31 +0530

    video reg

---
+++ using imageai/object-detection-video.py
@@ -0,0 +1,15 @@
+
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "funny_dog.mp4"),
+                                output_file_path=os.path.join(execution_path, "detected_video.mp4")
+                                , frames_per_second=20, log_progress=True)
+print(video_path)
